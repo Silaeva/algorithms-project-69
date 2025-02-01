6d6ec886ad49f3147a4b91ce96d941c8fd80b691
Silaeva Anna <silaeva.annushka@gmail.com>
2025-02-01 16:28:09 +0100

    add clean text

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+const cleanText = (text) => text.replace(/[.,!?;:()"-]/g, " ").toLowerCase().replace(/\s+/g, " ").trim();
+
 const search = (docsArray, searchString) => {
-  if (!searchString) return [];
+  if (!searchString.trim()) return [];
+
+  const cleanSearchString = cleanText(searchString);
+  if (!docsArray || !Array.isArray(docsArray) || !cleanSearchString) return [];
+
   return docsArray.reduce((acc, doc) => {
-    if (doc.text.toLowerCase().includes(searchString.toLowerCase())) {
+    if (cleanText(doc.text).includes(cleanSearchString)) {
       acc.push(doc.id);
     }
     return acc;
--- src/index.test.js
+++ src/index.test.js
@@ -1,34 +1,41 @@
-import search from './index';
+import search from "./index.js";
 
-describe('search function', () => {
+describe("search function", () => {
   const documents = [
-    { id: 1, text: 'Hello world' },
-    { id: 2, text: 'Jest is great' },
-    { id: 3, text: 'Unit tests are important' },
-    { id: 4, text: 'Hello again' },
+    { id: 1, text: "Hello world" },
+    { id: 2, text: "Jest is great" },
+    { id: 3, text: "Unit tests are important" },
+    { id: 4, text: "Hello again" },
+    { id: 5, text: "Find-me if you can..." },
   ];
 
-  test('finds documents containing the search string', () => {
-    expect(search(documents, 'hello')).toEqual([1, 4]);
+  test("finds documents containing the search string", () => {
+    expect(search(documents, "hello")).toEqual([1, 4]);
   });
 
-  test('returns an empty array if no matches are found', () => {
-    expect(search(documents, 'notfound')).toEqual([]);
+  test("returns an empty array if no matches are found", () => {
+    expect(search(documents, "notfound")).toEqual([]);
   });
 
-  test('is case insensitive', () => {
-    expect(search(documents, 'JEST')).toEqual([2]);
+  test("is case insensitive", () => {
+    expect(search(documents, "JEST")).toEqual([2]);
   });
 
-  test('matches substrings within words', () => {
-    expect(search(documents, 'test')).toEqual([3]);
+  test("matches substrings within words", () => {
+    expect(search(documents, "test")).toEqual([3]);
   });
 
-  test('returns all matching documents', () => {
-    expect(search(documents, 're')).toEqual([2, 3]);
+  test("returns all matching documents", () => {
+    expect(search(documents, "re")).toEqual([2, 3]);
   });
 
-  test('returns an empty array if search string is empty', () => {
-    expect(search(documents, '')).toEqual([]);
+  test("returns an empty array if search string is empty", () => {
+    expect(search(documents, "")).toEqual([]);
+  });
+
+  test("ignores punctuation in documents and search queries", () => {
+    expect(search(documents, "Hello!")).toEqual([1, 4]);
+    expect(search(documents, "unit tests")).toEqual([3]);
+    expect(search(documents, "Find me?")).toEqual([5]);
   });
 });
